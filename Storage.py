ff6b6d94e4e6286d700023210847b9b7c999695c
nmendoza <vmend001@odu.edu>
2019-05-29 16:59:51 -0400

    Adding basic functionality to insert and display all journal entries.

--- Storage.py
+++ Storage.py
@@ -2,23 +2,28 @@ import sqlite3
 from datetime import date
 
 journal_database = "journal.db"
-
 current_date = date.today().strftime("%m/%d/%Y")
-
 connection = sqlite3.connect(journal_database)
-
 cursor = connection.cursor()
-
 # Create table
-cursor.execute('''CREATE TABLE journal
+cursor.execute('''CREATE TABLE IF NOT EXISTS journal
              (date text, title text, journaltext text)''')
 
-# Insert a row of data
-note_title = "My First Journal entry"
-note_text = "My first journal entry's text"
-journal_values = (current_date, note_title, note_text,)
-cursor.execute('INSERT INTO journal VALUES (?,?,?)', journal_values)
 
-connection.commit()
+def add_new_entry(entry_note_title, entry_note_text):
+    journal_values = (current_date, entry_note_title, entry_note_text,)
+    cursor.execute('INSERT INTO journal VALUES (?,?,?)', journal_values)
+    connection.commit()
+
+
+def display_all_entries():
+    all_entries = cursor.execute('SELECT * FROM journal').fetchall()
+
+    print "=====All entries displayed here======"
+    for entry in all_entries:
+        print entry
+
+
+display_all_entries()
 
 connection.close()
